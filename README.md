a5e23418028640afef6230c365f363de36c7f31a
Mark Orel <mail.ormark@gmail.com>
2017-04-26 18:16:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+#AWS-Response
+
+## Purpose
+
+Lib for handling aws requests in single way
+
+### Use
+
+Include to your project as git dependency
+
+Use default exported function in next way
+
+```
+const R = required('aws-response');
+
+module.exports.lambda = R((data) => {
+    return { ok: true }; // = 200 status
+    
+    // or
+    // return Promise.resolve({ ok: true }); // = 200 status
+    
+    // or
+    // return Promise.reject(new Error('Some error')); // = 500 status and correct errorMessage
+    
+    // or
+        // return Promise.reject({ data:false }); // = 500 status and UnkownError name and message
+    
+    // or
+    // throw Error('Some error'); // will be handled as error and status = 500
+});
+```
+
+
+## Responses
+
+All responses have next shape and returned in compatible format with AWS Lambda proxy integration
+
+```
+{
+    statusCode: 200 || 500,
+    header: { Access-Control-Allow-Origin: '*' },
+    body: 'string'
+}
+```
+
+### Success body
+ 
+```
+{
+  "data": {
+    "ok": 1
+  }, 
+  "dataAvailable": true, 
+  "executionTimeInMs": 6, 
+  "originalRequest": {
+    "param1": "ok", 
+    "param2": "okok"
+  }, 
+  "requestId": 12345, 
+  "status": "success"
+}
+```
+
+### Error body
+
+```
+{
+  "dataAvailable": false, 
+  "errorData": null, 
+  "errorMessage": "PANIC", 
+  "errorName": "Error", 
+  "executionTimeInMs": 4, 
+  "originalRequest": {
+    "data": "ok", 
+    "param2": "2"
+  }, 
+  "requestId": 12345, 
+  "status": "error"
+}
+```
+
+### Development
+
+Please check package.json for commands.
+
+Be sure that `npm test` and `npm run lint` passes.
---
+++ test/index.test.js
@@ -0,0 +1,144 @@
+const { expect } = require('chai');
+const R = require('../lib/index');
+
+describe('Test handling request', () => {
+  it('should check if get params parsed correctly', (cb) => {
+    const event = require('./sample-requests/GET-request-aws.json');
+
+    const handler = R((data) => {
+      expect(data).to.be.deep.equal(event.queryStringParameters);
+      return { ok: 1 };
+    });
+
+    handler(event, { requestId: 12345 }, (err, result) => {
+      expect(result).to.have.all.keys(['statusCode', 'body', 'headers']);
+      expect(result.headers).to.be.an('object');
+      expect(result.headers).to.have.all.keys('Access-Control-Allow-Origin');
+      expect(result.statusCode).to.be.equal(200);
+      expect(result.body).to.be.a('string');
+      const response = JSON.parse(result.body);
+      expect(response).to.be.not.null;
+      expect(response).to.have.all.keys(['data', 'dataAvailable', 'executionTimeInMs', 'originalRequest', 'status', 'requestId']);
+      expect(response.data.ok).to.be.equal(1);
+      expect(response.originalRequest).to.be.deep.equal(
+        event.queryStringParameters
+      );
+      expect(response.dataAvailable).to.be.equal(true);
+      expect(response.executionTimeInMs).to.be.a('number');
+      expect(response.status).to.be.equal('success');
+      expect(response.requestId).to.be.equal(12345);
+      cb();
+    });
+  });
+
+  it('should check if POST params parsed correctly', (cb) => {
+    const event = require('./sample-requests/POST-request-aws.json');
+
+    const handler = R((data) => {
+      expect(data).to.be.deep.equal(JSON.parse(event.body));
+      return Promise.resolve({ ok: 1 });
+    });
+
+    handler(event, { requestId: 12345 }, (err, result) => {
+      expect(result).to.have.all.keys(['statusCode', 'body', 'headers']);
+      expect(result.headers).to.be.an('object');
+      expect(result.headers).to.have.all.keys('Access-Control-Allow-Origin');
+      expect(result.statusCode).to.be.equal(200);
+      expect(result.body).to.be.a('string');
+      const response = JSON.parse(result.body);
+      expect(response).to.be.not.null;
+      expect(response).to.have.all.keys(['data', 'dataAvailable', 'executionTimeInMs', 'originalRequest', 'status', 'requestId']);
+      expect(response.data.ok).to.be.equal(1);
+      expect(response.originalRequest).to.be.deep.equal(
+        JSON.parse(event.body)
+      );
+      expect(response.dataAvailable).to.be.equal(true);
+      expect(response.executionTimeInMs).to.be.a('number');
+      expect(response.status).to.be.equal('success');
+      expect(response.requestId).to.be.equal(12345);
+      cb();
+    });
+  });
+
+  it('should handle thrown error', (cb) => {
+    const event = require('./sample-requests/POST-request-aws.json');
+
+    const handler = R((data) => {
+      throw Error('PANIC');
+    });
+
+    handler(event, { requestId: 12345 }, (err, result) => {
+      expect(result).to.have.all.keys(['statusCode', 'body', 'headers']);
+      expect(result.headers).to.be.an('object');
+      expect(result.headers).to.have.all.keys('Access-Control-Allow-Origin');
+      expect(result.statusCode).to.be.equal(500);
+      expect(result.body).to.be.a('string');
+      const response = JSON.parse(result.body);
+      expect(response).to.be.not.null;
+      expect(response).to.have.all.keys(['errorData', 'errorMessage','errorName', 'dataAvailable', 'executionTimeInMs', 'originalRequest', 'status', 'requestId']);
+      expect(response.originalRequest).to.be.deep.equal(
+        JSON.parse(event.body)
+      );
+      expect(response.dataAvailable).to.be.equal(false);
+      expect(response.executionTimeInMs).to.be.a('number');
+      expect(response.status).to.be.equal('error');
+      expect(response.requestId).to.be.equal(12345);
+      cb();
+    });
+  });
+
+  it('should handle rejected promise', (cb) => {
+    const event = require('./sample-requests/POST-request-aws.json');
+
+    const handler = R((data) => {
+      return Promise.reject('PANIC');
+    });
+
+    handler(event, { requestId: 12345 }, (err, result) => {
+      expect(result).to.have.all.keys(['statusCode', 'body', 'headers']);
+      expect(result.headers).to.be.an('object');
+      expect(result.headers).to.have.all.keys('Access-Control-Allow-Origin');
+      expect(result.statusCode).to.be.equal(500);
+      expect(result.body).to.be.a('string');
+      const response = JSON.parse(result.body);
+      expect(response).to.be.not.null;
+      expect(response).to.have.all.keys(['errorData', 'errorMessage','errorName', 'dataAvailable', 'executionTimeInMs', 'originalRequest', 'status', 'requestId']);
+      expect(response.originalRequest).to.be.deep.equal(
+        JSON.parse(event.body)
+      );
+      expect(response.dataAvailable).to.be.equal(false);
+      expect(response.executionTimeInMs).to.be.a('number');
+      expect(response.status).to.be.equal('error');
+      expect(response.requestId).to.be.equal(12345);
+      cb();
+    });
+  });
+
+
+  it('should handle rejected error in promise', (cb) => {
+    const event = require('./sample-requests/POST-request-aws.json');
+
+    const handler = R((data) => {
+      return Promise.reject(new Error('PANIC'));
+    });
+
+    handler(event, { requestId: 12345 }, (err, result) => {
+      expect(result).to.have.all.keys(['statusCode', 'body', 'headers']);
+      expect(result.headers).to.be.an('object');
+      expect(result.headers).to.have.all.keys('Access-Control-Allow-Origin');
+      expect(result.statusCode).to.be.equal(500);
+      expect(result.body).to.be.a('string');
+      const response = JSON.parse(result.body);
+      expect(response).to.be.not.null;
+      expect(response).to.have.all.keys(['errorData', 'errorMessage','errorName', 'dataAvailable', 'executionTimeInMs', 'originalRequest', 'status', 'requestId']);
+      expect(response.originalRequest).to.be.deep.equal(
+        JSON.parse(event.body)
+      );
+      expect(response.dataAvailable).to.be.equal(false);
+      expect(response.executionTimeInMs).to.be.a('number');
+      expect(response.status).to.be.equal('error');
+      expect(response.requestId).to.be.equal(12345);
+      cb();
+    });
+  });
+});
